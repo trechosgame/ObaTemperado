12ac08c3c97a3a158248d38b69e50ecd003483ad
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-12-09 13:51:32 -0300

    Update 02 - SelectDistinct.sql

--- 02 - SelectDistinct.sql
+++ 02 - SelectDistinct.sql
@@ -1,4 +1,4 @@
-Com relação à tabela TURFE, descrita anteriormente, analise o comando SQL a seguir.
+/* Com relação à tabela TURFE, descrita anteriormente, analise o comando SQL a seguir. */
 
 select distinct cavalo
 from TURFE t1
@@ -8,4 +8,4 @@ where not exists
          and t3.pareo = t2.pareo
          and t3.tempo > t2.tempo)
 
-A execução do comando acima produz, somente, todos os cavalos que, em suas corridas
+/* A execução do comando acima produz, somente, todos os cavalos que, em suas corridas */
