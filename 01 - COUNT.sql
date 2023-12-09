5ada6f74a741ffee0f30a74e47e664d21d381464
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-12-08 21:45:48 -0300

    Create 01 - COUNT.sql

---
+++ 01 - COUNT.sql
@@ -0,0 +1,6 @@
+Para exibir o número de diferentes nomes de departamentos (nomeDepartamento) a partir da
+tabela TabDepartamentos, suprimindo duplicacões, utiliza-se a instrução SQL:
+SELECT
+
+
+COUNT (nomeDepartamento) FROM TabDepartamentos NOT REPEAT
