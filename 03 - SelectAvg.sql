29d578d4a657c84c210e9b7d89effb8f8f1d39ce
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-12-08 22:14:13 -0300

    Create 03 - SelectAvg.sql

---
+++ 03 - SelectAvg.sql
@@ -0,0 +1,11 @@
+Com relação à tabela TURFE, descrita anteriormente, analise o comando SQL a seguir.
+
+select
+  cavalo,
+  (select avg(tempo)
+    from TURFE t
+    where t.cavalo = TURFE.cavalo) media
+from TURFE
+
+O número de linhas do resultado produzido pela execução do comando acima, executada 
+  linha de titulo, é
