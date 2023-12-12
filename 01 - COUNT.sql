babca51f1086a1e3d0d7034192c35cfba7190d53
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-12-12 20:39:18 -0300

    Delete 01 - COUNT.sql

--- 01 - COUNT.sql
+++
@@ -1,5 +0,0 @@
-/* Para exibir o número de diferentes nomes de departamentos (nomeDepartamento) a partir da
-tabela TabDepartamentos, suprimindo duplicacões, utiliza-se a instrução SQL:
-SELECT */
-
-COUNT(DISTINCT nomeDepartamento) FROM TabDepartamentos;
